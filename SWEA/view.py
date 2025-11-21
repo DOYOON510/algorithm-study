98e5e9b2416a271e72e704e4eedd52e72160bccc
Doyoon Kim <doyoon510@gmail.com>
2025-11-22 05:24:41 +0900

    구현

--- SWEA/view.py
+++ SWEA/view.py
@@ -1,14 +1,15 @@
-# import sys
-# sys.stdin = open("input.txt", "r")
-for test_case in range(1, 11):
-    n=int(input())
-    house=list(map(int,input().split()))
-    result=0
-    for i in range(2,len(house)-2):
-        r1=house[i]-house[i+1]
-        r2=house[i]-house[i+2]
-        l1=house[i]-house[i-1]
-        l2=house[i]-house[i-2]
-        if r1>0 and r2>0 and l1>0 and l2>0:
-            result+=min(r1,r2,l1,l2)
-    print('#{} {}'.format(test_case,result))
+
+for test_case in range(1, 3):
+    n = int(input())
+    lst = list(map(int, input().split()))
+    rst = 0
+
+    for i in range(2, n-2):
+        lst2 = []
+        lst2.append(lst[i] - lst[i - 1])
+        lst2.append(lst[i] - lst[i - 2])
+        lst2.append(lst[i] - lst[i + 1])
+        lst2.append(lst[i] - lst[i + 2])
+        if min(lst2) > 0:
+            rst += min(lst2)
+    print('#{} {}'.format(test_case, rst))
---
+++ 이코테/구현/문자열 압축.py
@@ -0,0 +1,21 @@
+
+
+def solution(s):
+    answer = len(s)
+
+    for i in range(1, (len(s)//2)+1):
+        com = ""
+        prev = s[0:i]
+        cnt = 1
+
+        for j in range(i, len(s), i):
+            if prev == s[j:j+i]:
+                cnt +=1
+            else:
+                com += str(cnt)+prev if cnt >= 2 else prev
+                prev = s[j:j+i]
+                cnt =1
+                print(i, j, prev)
+        com += str(cnt)+prev if cnt >= 2 else prev
+        answer = min(answer, len(com))
+    return answer
