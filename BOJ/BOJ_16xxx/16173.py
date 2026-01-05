eb13d4158cfe51c708979ba0ba5a519edfb52a72
Doyoon Kim <doyoon510@gmail.com>
2026-01-05 18:08:20 +0900

    실버4

---
+++ BOJ/BOJ_16xxx/16173.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+n = int(input())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+visited = []
+for _ in range(n):
+    visited.append([False]*n)
+
+dx = [0, 1]
+dy = [1, 0]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+
+    while queue:
+        x, y = queue.popleft()
+
+        if graph[x][y] == -1:
+            return 'HaruHaru'
+
+        jump = graph[x][y]
+        for i in range(2):
+            nx = x + dx[i] * jump
+            ny = y + dy[i] * jump
+
+            if nx>=0 and nx<n and ny>=0 and ny<n and visited[nx][ny] == 0:
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+    return 'Hing'
+
+print(bfs(0,0))
+
+
--- 이코테/DFS_BFS/미로 탈출.py
+++ 이코테/DFS_BFS/미로 탈출.py
@@ -6,6 +6,9 @@ lst = []
 for i in range(n):
     lst.append(list(map(int, input())))
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 def bfs(x, y):
     queue = deque()
     queue.append((x, y))
