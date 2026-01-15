8589acad60f167dced564044a0eb2097bf9aaba5
DOYOON510 <129147977+DOYOON510@users.noreply.github.com>
2026-01-15 19:00:47 +0900

    [level 2] Title: 문자열 압축, Time: 3.15 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,26 +1,24 @@
 def solution(s):
     ans = len(s)
-    
-    for i in range(1, len(s)//2 +1):
+    for i in range(1, int(len(s) / 2 + 1)):
         com = ''
         cnt = 1
-        prev = s[0:i]
-        
-        for j in range(i, len(s), i):
-            if prev == s[j:j+i]:
+        prev = s[:i]
+        for j in range(i, len(s)+1, i):
+            if prev == s[j:j + i]:
                 cnt += 1
             else:
                 if cnt > 1:
                     com += str(cnt) + prev
                 else:
                     com += prev
-                cnt = 1
                 prev = s[j:j+i]
+                cnt = 1
         if cnt > 1:
             com += str(cnt) + prev
         else:
             com += prev
-        
+
         ans = min(ans, len(com))
-    
+
     return ans
