a50a1fd9880293c009c122ba4c28527f423f58ea
DOYOON510 <129147977+DOYOON510@users.noreply.github.com>
2026-01-23 20:53:26 +0900

    [Silver II] Title: 랜선 자르기, Time: 324 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,15 +1,19 @@
-k, n = map(int, input().split())
-lst = list(int(input()) for _ in range(k))
+n, m = map(int, input().split())
+lst = list(int(input()) for i in range(n))
+
 start, end = 1, max(lst)
 
 while start <= end:
-    mid = (end+start) // 2
+    mid = (start + end) // 2
     rst = 0
+
     for i in lst:
-        rst += i//mid
-    if rst >= n:
-        start = mid + 1
+        rst += i // mid
+
+    if rst >= m:
+        ans = mid
+        start = mid +1
     else:
         end = mid - 1
 
-print(end)
+print(ans)
